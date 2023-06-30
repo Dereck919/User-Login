63e30d20685fc6887fd6a8ab7bbe325103760be1
Dereck919 <kirkmandereck@gmail.com>
2023-06-30 02:45:37 -0700

    changes

--- src/main.cpp
+++ src/main.cpp
@@ -4,42 +4,88 @@
 //Login, Logout, change username, change password, create new account
 //have data stored, have that data read 
 
-//default user
+//User datastructure
+struct User{
+    std::string username;
+    std::string password;
+};
+
+//function to create and store a new account 
+void create_account(){
+    system("clear");
+    
+    std::string temp_user = " ";
+    std::string temp_pass = " ";
+
+    std::cout << "Enter username: ";
+    std::cin >> temp_user;
+
+    std::cout << "Enter password: ";
+    std::cin >> temp_pass;
+    
+    //Create new user of type user
+    User user;
+
+    user.username = temp_user;
+    user.password = temp_pass;
+
+    system("clear");
+    std::cout << "Entering new user into databse" << std::endl;
+
+    std::fstream fs;
+    fs.open("database.dat");
+    
+    //Insert new user into databse 
+
+    fs << user.username << " " << user.password << std::endl << "Test";
+
+};
+
+//Test user
 std::string admin_user= "admin123";
 std::string admin_pass = "adminpass";
 
-//What happens during a login success
 void login_success(){
     system("clear");
     std::cout << "Login successful" << std::endl;
 }
 
-//What happens during a login failure
 void login_failure(){
     system("clear");
     std::cout << "Incorrect username or password " << std::endl;
     std::cout << "1. Exit" << std::endl;
     std::cout << "2. Forgot password" << std::endl;
-}
 
-//function to login 
+    int choice;
+
+    std::cin >> choice;
+
+    if (choice == 1){
+
+    } else if (choice == 2){
+        forgot_password();
+    } else {
+        std::cout << "invalid input";
+    }
+}
+ 
 void login(){
     system("clear");
-    std::cout << "Enter Username: " << std::endl;
     std::string check_user = " ";
+    std::string check_pass = " ";
+
+    std::cout << "Enter Username: " << std::endl;
     std::cin >> check_user;
-    if (check_user == admin_user){
+
+    std::cout << "Enter Password: " << std::endl;
+    std::cin >> check_pass;
+
+    if (check_user == admin_user && check_pass == admin_pass){
         login_success();
     } else {
         login_failure();
     }
 }
-
-//function to create and store a new account 
-void create_account(){
-    system("clear");
-    std::cout << "account" << std::endl;
-};
 
 void forgot_password(){
     system("clear");
@@ -47,6 +93,7 @@ void forgot_password(){
 };
 
 void display_home(){
+    system("clear");
     std::cout << "1. Login" << std::endl;
     std::cout << "2. Create User" << std::endl;
     std::cout << "3. Forgot password" << std::endl; 
@@ -63,8 +110,10 @@ void display_home(){
             break;
         case 3:
             forgot_password();
+            break;
         default:
-            std::cout << "Invalid Character" << std::endl;
+            //Loop back if invalid input is given
+            display_home();
     }
 }
 
--- src/main.hpp
+++ src/main.hpp
@@ -1,17 +1,24 @@
 #include <iostream>
 #include <fstream>
 
+struct user{
+    std::string username;
+    std::string password;
+};
+
 void display_home();
 
-//Done
+//Wip
 void login();
 
 void create_account();
+
+void parse_dat();
 
 void forgot_password();
 
 //Done
 void login_success();
 
-//Done
+//Wip
 void login_failure();
